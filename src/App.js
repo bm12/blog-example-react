836fc64578f56b0e9b3979601b5b740680db8cdb
bm12 <mahmud1997babaev@mail.ru>
2018-05-23 16:40:46 +0300

    add axios

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  componentDidMount() {
+    console.log(axios.get('https://blog-example-bm12.herokuapp.com:3000/posts'));
+  }
+
   render() {
+    console.log('render');
     return (
       <div className="App">
         <header className="App-header">
