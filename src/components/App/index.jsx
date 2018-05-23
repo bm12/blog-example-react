3a496b51f9da0911d64e12f106a91fcc299c941d
bm12 <mahmud1997babaev@mail.ru>
2018-05-24 01:03:40 +0300

    Add blog template. and sass compiler

---
+++ src/components/App/index.jsx
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import './style.css';
+
+
+class App extends Component {
+  state = {
+    posts: [],
+  }
+  componentDidMount() {
+    // axios.get('http://lvh.me:5000/posts?_page=1&_limit=12').then(res => {
+    axios.get('./posts?_page=1&_limit=12').then(res => {
+      this.setState({ posts: res.data });
+      console.log(res);
+    });
+  }
+
+  render() {
+    const { posts } = this.state;
+    console.log('render');
+    return (
+      <div className="wraper">
+        <header className="header">
+          <div className="container">
+            <div className="header__logo"> blog-example </div>
+          </div>
+        </header>
+        <main className="main-page">
+          <div className="banner">
+            <div className="container">
+              <h1 className="banner__title"> blog-exapmle </h1>
+              <p className="banner__sub-title">Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+            </div>
+          </div>
+          <div className="blogs-list">
+            <div className="container">
+              <div className="row no-gutters">
+                {posts.map((post) => {
+                  return (
+                    <div key={post.id} className="col-md-4 blogs-list__item">
+                      <div className="blog-card">
+                        <header className="blog-card__head">
+                          <img className="img-fluid blog-card__img" src={`https://placeimg.com/640/480/arch?${post.id}`} width="640" height="480" alt="" />
+                          <div className="blog-card__category">Architect {post.id}</div>
+                        </header>
+                        <div className="blog-card__content">
+                          <h5 className="blog-card__author">Leanne Graham:</h5>
+                          <p className="blog-card__title">{post.title}</p>
+                        </div>
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          </div>
+        </main>
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+
+import './style/fonts.css';
+import './style/bootstrap/bootstrap-reboot.css';
+import './style/bootstrap/bootstrap-grid.css';
+// import './style/bootstrap/bootstrap.css';
+
+import App from './components/App';
 // import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
