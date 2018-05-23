b58abe5eda9509cf3a640ed15784394d42cf2475
bm12 <mahmud1997babaev@mail.ru>
2018-05-23 18:44:09 +0300

    change server.js

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+const middlewares = jsonServer.defaults({ static: './build' })
 const PORT = process.env.PORT || 5000
 
 // Set default middlewares (logger, static, cors and no-cache)
 server.use(middlewares)
 
 // Add custom routes before JSON Server router
-server.get('/', (req, res) => {
-    console.log('req /', req)
-  res.end('./build/index.html');
-})
+// server.get('/', (req, res) => {
+//     console.log('req /', req)
+//   res.end('./build/index.html');
+// })
 
 // To handle POST, PUT and PATCH you need to use a body-parser
 // You can use the one used by JSON Server
