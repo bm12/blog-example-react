35f8f1fbe2dcecb4f36b65153878e3e53be9f968
bm12 <mahmud1997babaev@mail.ru>
2018-06-17 17:00:36 +0300

    remove <Router> from index.js to App component

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Redirect, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 import Header from '../Header';
 import MainPage from '../MainPage';
 import './style.css';
@@ -8,14 +8,16 @@ import PostPage from '../PostPage';
 
 function App() {
     return (
-        <div className="wraper">
-            <Header />
-            <Switch>
-                <Route exact path='/' render={(props) => <Redirect to='/main-page/1' {...props} />} />
-                <Route path='/main-page/:pageId' component={MainPage} />
-                <Route path='/post-page/:postId' component={PostPage} />
-            </Switch>
-        </div>
+        <Router>
+            <div className="wraper">
+                <Header />
+                <Switch>
+                    <Route exact path='/' render={(props) => <Redirect to='/main-page/1' {...props} />} />
+                    <Route path='/main-page/:pageId' component={MainPage} />
+                    <Route path='/post-page/:postId' component={PostPage} />
+                </Switch>
+            </div>
+        </Router>
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
 
 import './style/css/fonts.css';
 import './style/css/bootstrap/bootstrap-reboot.css';
@@ -10,9 +9,5 @@ import './style/css/bootstrap/bootstrap-grid.css';
 import App from './components/App';
 // import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render((
-    <Router>
-        <App />
-    </Router>
-), document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 // registerServiceWorker();
