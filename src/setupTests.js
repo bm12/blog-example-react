410e570ab0e546174dfc5c451da32fc3667dd083
bm12 <mahmud1997babaev@mail.ru>
2018-06-22 22:51:05 +0300

    change setupTests for throw erron on console.error()

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,4 +1,8 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
+console.error = message => {
+    throw new Error(message);
+};
+
 configure({ adapter: new Adapter() });
