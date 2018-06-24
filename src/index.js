055cb7ac52e8c8f7e4ec5c59ef2ca8b106f3045b
bm12 <mahmud1997babaev@mail.ru>
2018-06-24 18:29:27 +0300

    add redux & react-redux package and add some init code in src/index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import reducers from './reducers';
 
 import './style/css/fonts.css';
 import './style/css/bootstrap/bootstrap-reboot.css';
@@ -9,6 +12,15 @@ import './style/css/bootstrap/bootstrap-grid.css';
 import App from './components/App';
 // import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+    reducers,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+document.getElementById('root'));
 // TODO: add registerSW after end development
 // registerServiceWorker();
