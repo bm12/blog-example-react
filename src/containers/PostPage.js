3ac3c52bec60ea9e4cea6197094c3a9c6858cbb7
bm12 <mahmud1997babaev@mail.ru>
2018-07-12 22:01:02 +0300

    add formatedPostSelector

--- src/containers/PostPage.js
+++ src/containers/PostPage.js
@@ -1,16 +1,16 @@
 import { connect } from "react-redux";
 import Component from "../components/PostPage";
 import { fetchPostAndUserById } from "../actions";
+import { formatedPostSelector } from "../selectors";
 
 const mapStateToProps = (state, { match }) => {
     const { postId } = match.params;
     const post = state.postPage[postId];
-    
+
     if (!post) return { loaded: false };
 
-    const postText = post.body.replace(/\n/g, '<br />');
-    const newPost = { ...post, body: postText };
-    
+    const newPost = formatedPostSelector(post);
+
     return { post: newPost, loaded: true };
 };
 
--- src/selectors/index.js
+++ src/selectors/index.js
@@ -5,3 +5,11 @@ export const postsSelector = createSelector(
     getPosts,
     posts => Object.values(posts),
 );
+
+export const formatedPostSelector = createSelector(
+    post => post,
+    post => {
+        const postText = post.body.replace(/\n/g, '<br />');
+        return { ...post, body: postText };
+    },
+);
