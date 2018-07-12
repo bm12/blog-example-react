00c3451afebcf802c55c4ebe61994d9df2234452
bm12 <mahmud1997babaev@mail.ru>
2018-07-12 21:39:49 +0300

    delete useless selector

--- src/containers/MainPage.js
+++ src/containers/MainPage.js
@@ -1,12 +1,11 @@
 import { connect } from "react-redux";
 import Component from "../components/MainPage";
-import { postsSelector, usersSelector } from "../selectors";
+import { postsSelector } from "../selectors";
 import { fetchPosts } from "../actions";
 
 const mapStateToProps = (state) => ({
     posts: postsSelector(state),
     postsCount: state.postsCount,
-    users: usersSelector(state),
 });
 
 export default connect(mapStateToProps, { fetchPosts })(Component);
--- src/selectors/index.js
+++ src/selectors/index.js
@@ -5,9 +5,3 @@ export const postsSelector = createSelector(
     getPosts,
     posts => Object.values(posts),
 );
-
-const getUsers = (state) => state.users;
-export const usersSelector = createSelector(
-    getUsers,
-    users => Object.values(users),
-);
