398b45f501ac43707eeb3d83da356fc38825753d
bm12 <mahmud1997babaev@mail.ru>
2018-07-13 14:16:47 +0300

    added routes.getPostUrl(postId)

--- src/helpers/urls.js
+++ src/helpers/urls.js
@@ -5,6 +5,7 @@ routes.mainPageLink = '/main-page';
 routes.postPageLink = '/post-page';
 
 routes.getPageUrl = (page = 1) => `${routes.hostname}/api/posts?_page=${page}&_limit=12&_embed=images`;
+routes.getPostUrl = (postId) => `${routes.hostname}/api/posts/${postId}?_embed=images`;
 routes.getUsersUrl = (idsString = '' ) => `${routes.hostname}/api/users?${idsString}`;
 routes.getPostWithImgAndUser = (post = 1) => `${routes.hostname}/api/posts/${post}?_embed=images&_expand=user`;
 
