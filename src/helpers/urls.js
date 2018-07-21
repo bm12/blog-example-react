495ad636063d1c3ad4f16d3dab670a77b4583812
bm12 <mahmud1997babaev@mail.ru>
2018-07-21 14:24:14 +0300

    add routes.getCommentsUrl

--- src/helpers/urls.js
+++ src/helpers/urls.js
@@ -8,5 +8,6 @@ routes.getPageUrl = (page = 1) => `${routes.hostname}/api/posts?_page=${page}&_l
 routes.getPostUrl = (postId) => `${routes.hostname}/api/posts/${postId}?_embed=images`;
 routes.getUsersUrl = (idsString = '' ) => `${routes.hostname}/api/users?${idsString}`;
 routes.getPostWithImgAndUser = (post = 1) => `${routes.hostname}/api/posts/${post}?_embed=images&_expand=user`;
+routes.getCommentsUrl = (postId) => `${routes.hostname}/api/comments?postId=${postId}`;
 
 export default routes;
