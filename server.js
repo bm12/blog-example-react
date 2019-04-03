0f761a79a86ebb910ad421d507cacddfdc78cadf
Babaev Makhmud <mahmud1997babaev@gmail.com>
2019-04-03 15:06:18 +0300

    Add https

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
+const https = require('https');
+const fs = require('fs');
+const path = require('path');
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
 const entryDir = './build';
-const middlewares = jsonServer.defaults({ static: entryDir })
-const PORT = process.env.PORT || 5000
+const middlewares = jsonServer.defaults({ static: entryDir });
+const PORT = process.env.PORT || 5000;
 
 // Set default middlewares (logger, static, cors and no-cache)
-server.use(middlewares)
+server.use(middlewares);
 
 // Add custom routes before JSON Server router
 // server.get('/:request', (req, res) => {
@@ -21,23 +24,32 @@ server.use((req, res, next) => {
   if (req.method === 'GET' && !(/\/api/.test(req.url))) {
     res.sendFile(`${__dirname}/${entryDir}/index.html`)
   } else {
-    next()
+    next();
   }
-})
+});
 
 // To handle POST, PUT and PATCH you need to use a body-parser
 // You can use the one used by JSON Server
-server.use(jsonServer.bodyParser)
+server.use(jsonServer.bodyParser);
 server.use((req, res, next) => {
   if (req.method === 'POST') {
-    req.body.createdAt = Date.now()
+    req.body.createdAt = Date.now();
   }
   // Continue to JSON Server router
-  next()
-})
+  next();
+});
 
 // Use default router
-server.use('/api', router)
-server.listen(PORT, () => {
-  console.log('JSON Server is running')
-})
+server.use('/api', router);
+// server.listen(PORT, () => {
+//   console.log('JSON Server is running');
+// });
+
+const httpsOptions = {
+  key: fs.readFileSync(path.resolve(__dirname, './server/cert/localhost.key')),
+  cert: fs.readFileSync(path.resolve(__dirname, './server/cert/localhost.cert')),
+};
+
+https.createServer(httpsOptions, server).listen(PORT, () => {
+  console.log('JSON Server is running');
+});
